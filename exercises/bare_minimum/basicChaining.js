1e64ba0719ec96dd0c4c29a0f7c5a381199c030b
Aslambek Baimouradov <zandak@gmail.com>
2023-07-25 14:47:46 -0400

    Completed bare minimums

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -2,7 +2,9 @@
  * Write a function WITH NO CALLBACKS that,
  * (1) reads a GitHub username from a `readFilePath`
  *     (the username will be the first line of the file)
+ * promiseConstructorFunctions
  * (2) then, sends a request to the GitHub API for the user's profile
+ * // getGitHubProfile
  * (3) then, writes the JSON response of the API to `writeFilePath`
  *
  * HINT: We exported some similar promise-returning functions in previous exercises
@@ -10,50 +12,61 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
+var promiseConstructorFunctions = require('./promiseConstructor');
+var pluckFirstLineFromFileAsync = promiseConstructorFunctions.pluckFirstLineFromFileAsync;
+var promisifyFunctions = require('./promisification');
+var getGitHubProfileAsync = promisifyFunctions.getGitHubProfileAsync;
+// var fsWriteFileAsyn = ;
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(readFilePath, 'utf8', function(err, data) {
-      if(err) {
-        reject(err);
-      } else {
-        var username = data.trim();
-        var options = {
-          hostname: 'api.github.com',
-          path: '/users/' + username,
-          method: 'GET',
-          headers: {
-            'User-Agent': 'MyApp'
-          }
-        };
-        var req = https.request(options, function (response) {
-          var responseData = '';
-
-          response.on('data', function (chunk) {
-            responseData += chunk;
-          });
-          response.on('end', function () {
-
-            fs.writeFile(writeFilePath, JSON.stringify(JSON.parse(responseData), null, 2), function (err) {
-              if (err) {
-                reject(err);
-              } else {
-                resolve('Profile data written successfully to ' + writeFilePath);
-              }
-            });
-          });
-        });
-
-        req.on('error', function (error) {
-          reject(error);
-        });
-
-        req.end();
-      }
+  return pluckFirstLineFromFileAsync(readFilePath)
+    .then((username) => {
+      return getGitHubProfileAsync(username);
+    })
+    .then((body) => {
+      return Promise.promisify(fs.writeFile)(writeFilePath, JSON.stringify(body));
     });
-  });
+  // return new Promise(function(resolve, reject) {
+  //   fs.readFile(readFilePath, 'utf8', function(err, data) {
+  //     if (err) {
+  //       reject(err);
+  //     } else {
+  //       var username = data.trim();
+  //       var options = {
+  //         hostname: 'api.github.com',
+  //         path: '/users/' + username,
+  //         method: 'GET',
+  //         headers: {
+  //           'User-Agent': 'MyApp'
+  //         }
+  //       };
+  //       var req = https.request(options, function (response) {
+  //         var responseData = '';
+
+  //         response.on('data', function (chunk) {
+  //           responseData += chunk;
+  //         });
+  //         response.on('end', function () {
+
+  //           fs.writeFile(writeFilePath, JSON.stringify(JSON.parse(responseData), null, 2), function (err) {
+  //             if (err) {
+  //               reject(err);
+  //             } else {
+  //               resolve('Profile data written successfully to ' + writeFilePath);
+  //             }
+  //           });
+  //         });
+  //       });
+
+  //       req.on('error', function (error) {
+  //         reject(error);
+  //       });
+
+  //       req.end();
+  //     }
+  //   });
+  // });
 };
 
 // Export these functions so we can test them
--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -1,7 +1,7 @@
 /**
  * Create the promise returning `Async` suffixed versions of the functions below,
  * Promisify them if you can, otherwise roll your own promise returning function
- */ 
+ */
 
 var fs = require('fs');
 var request = require('needle');
@@ -29,7 +29,7 @@ var getGitHubProfile = function (user, callback) {
   });
 };
 
-var getGitHubProfileAsync; // TODO
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile); // TODO
 
 
 // (2) Asyncronous token generation
@@ -40,27 +40,34 @@ var generateRandomToken = function(callback) {
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken); // TODO
 
 
 // (3) Asyncronous file manipulation
 var readFileAndMakeItFunny = function(filePath, callback) {
   fs.readFile(filePath, 'utf8', function(err, file) {
     if (err) { return callback(err); }
-   
+
     var funnyFile = file.split('\n')
       .map(function(line) {
         return line + ' lol';
       })
       .join('\n');
 
-    callback(funnyFile);
+    callback(null, funnyFile);
+
   });
 };
+//console.log(readFileAndMakeItFunny());
+var readFileAndMakeItFunnyAsync = Promise.promisify(readFileAndMakeItFunny);
 
-var readFileAndMakeItFunnyAsync; // TODO
-
+// TODO
+// console.log('after promisify', readFileAndMakeItFunnyAsync);
 // Export these functions so we can test them and reuse them in later exercises
+
+
+
+
 module.exports = {
   getGitHubProfileAsync: getGitHubProfileAsync,
   generateRandomTokenAsync: generateRandomTokenAsync,
